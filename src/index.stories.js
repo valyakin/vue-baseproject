778597da9b4ed4c5d014f591cd41eb71dd19566f
Артем Валякин <aevalyakin@chel.rucard.ru>
2018-03-02 10:38:02 +0500

    Stories eslint fix

--- src/index.stories.js
+++ src/index.stories.js
@@ -1,21 +1,19 @@
-import { storiesOf } from '@storybook/vue';
-import { action } from '@storybook/addon-actions';
-
-import * as components from '../components';
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+import * as components from '../components'
 
 storiesOf('components', module)
   .add('button', () => ({
     components,
-    render(h) {
-      return <button-component onClick={this.logEvent} label="Нажать на кнопку"></button-component>;
+    render (h) {
+      return <button-component onClick={this.logEvent} label="Нажать на кнопку"></button-component>
     },
-    methods: { logEvent: action('clicked') },
+    methods: { logEvent: action('clicked') }
   }))
   .add('input', () => ({
     components,
-    render(h) {
-      return <input-component onInput={this.logEvent} label="Поле"></input-component>;
+    render (h) {
+      return <input-component onInput={this.logEvent} label="Поле"></input-component>
     },
-    methods: { logEvent: action('input') },    
+    methods: { logEvent: action('input') }
   }))
-
